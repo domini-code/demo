15b84c2a98c82dc70fb0d0516e5cc823dd0f865e
Bezael <bezael_e@hotmail.com>
2020-06-14 18:37:23 +0200

    Directive ngFor and ngIf

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  public users = [
+    { name: 'David', city: 'Barcelona' },
+    { name: 'Dominicode', city: '' },
+    { name: 'Bezael', city: 'Madrid' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
