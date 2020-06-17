33625dda47b2935a7ddb2ca1189afa5fe4afac96
Bezael <bezael_e@hotmail.com>
2020-06-17 20:18:28 +0200

    Add details

--- src/app/details-product/details-product.component.ts
+++ src/app/details-product/details-product.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-details-product',
   templateUrl: './details-product.component.html',
-  styleUrls: ['./details-product.component.scss']
+  styleUrls: ['./details-product.component.scss'],
 })
 export class DetailsProductComponent implements OnInit {
+  public product: any = {};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private dataSvc: DataService) {}
 
   ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    [this.product] = this.product = this.dataSvc.getProductById(productId);
+    console.log('Product->', this.product);
   }
-
 }
